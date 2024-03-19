090bdb6d2f537ffc7cc28f9f6fb7ef5f47e441af
Professor Otaviano Silvério <60519633+professorotavianosilverio@users.noreply.github.com>
2024-03-19 11:14:41 -0300

    Add files via upload

--- Algoritmo1_Decisao.java
+++ Algoritmo1_Decisao.java
@@ -1,30 +1,21 @@
-// Informando para o Java que necessito utilizar entrada de dados//
+//Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido
 import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
-    // criando uma variavel do tipo input para entrada de dados//
     Scanner teclado = new Scanner( System.in );
-    // Declarar as variaveis
-    int V1, V2, V3;
-    double media;
-    // Ler os valores
-    System.out.print("Digite o Valor 1: ");
-    V1 = teclado.nextInt();
-    System.out.print("Digite o Valor 2: ");
-    V2 = teclado.nextInt();
-    System.out.print("Digite o Valor 3: ");
-    V3 = teclado.nextInt();
-    // Calcular a media
-    media = (V1+V2+V3)/3;
-    // Exibir a media
-    System.out.println("A média é ->"+media);
-    if (media <= 10){
-       System.out.println("A média é menor que 10 "+media);
-    }else{
-       System.out.println("A média é maior que 10 "+media);
+    // Declaracao de variaveis
+    int nota=0;
+    System.out.println("Digite uma nota: ");
+    nota = teclado.nextInt();
+    while (nota > 10 || nota < 0){
+      
+      System.out.println("Nota invalida.Digite novamente");
+     
+      System.out.println("Digite uma nota: ");
+      nota = teclado.nextInt();
     }
-    
-    
-  }
+     System.out.println("Nota valida");
+   }
 
+  
 }
